e91a1989b2dcd9504356231a1d76b3b97618c524
felipeiop2 <felipeiop999@gmail.com>
2024-02-02 19:40:27 -0300

    ss

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 30.5
+console.log (`a velocidade do seu carro e ${velocidade} km por hora`)
+if(velocidade > 60){
+    console.log(`voce ultrapassou a velocidade permitida, multado`)
+} 
+console.log(`dirija sempre com cinto de seguranca`)
