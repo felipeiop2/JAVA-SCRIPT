8cbeabc6d8dff3587cca1717737020f1d6d21046
felipeiop2 <felipeiop999@gmail.com>
2024-02-04 12:21:58 -0300

    Horario do dia

--- desafio/script.js
+++ desafio/script.js
@@ -3,16 +3,19 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora sao ${hora} horas`
+    msg.innerHTML = `<strong>Agora sao ${hora} horas</strong>`
     if (hora >= 0 && hora < 12) {
         //bom dia
         img.src = 'imagens/dia.png'
+        document.body.style.background = '#00ffce'
     }else if (hora >= 12 && hora < 18){
         //boa tarde
         img.src = 'imagens/tarde.png'
+        document.body.style.background = '#bf7d75'
     } else {
         //boa noite
         img.src = 'imagens/noite.png'
+        document.body.style.background = '#666796'
     }
 
 
