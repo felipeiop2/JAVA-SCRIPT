8b51af747bae5a3ad604565975ff41a322b4b075
felipeiop2 <felipeiop999@gmail.com>
2024-02-05 14:29:01 -0300

    verificar de idade

---
+++ desafio2/script.js
@@ -0,0 +1,10 @@
+function verificar (){
+    var ano = window.document.getElementById('txt')
+    var masc = window.document.getElementById('masc')
+    var fem = window.document.getElementById('fem')
+    var img = window.document.getElementById('img')
+    if (ano = 2000){
+        img.src('imagem/bebehomem.png')  
+
+    }
+}
