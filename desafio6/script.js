6691bcc5ad0b6f1aaaf53b55b75387535beb1b82
felipeiop2 <felipeiop999@gmail.com>
2024-02-22 17:08:15 -0300

    teste

--- desafio6/script.js
+++ desafio6/script.js
@@ -1,2 +1,8 @@
 let numero = prompt('Me Fale um Numero')
+let numero2 = prompt(' Me Fale outro numero')
+let soma = parseInt(numero) + parseInt(numero2)
 let numerot = document.getElementById('numero')
+let res = document.getElementById('res')
+let res2 = document.getElementById('res2')
+numerot.innerHTML = numero
+res2.innerHTML = soma
