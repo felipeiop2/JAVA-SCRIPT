b4c47dfa0de83ea0842687e736bcb85f6590ecd1
felipeiop2 <felipeiop999@gmail.com>
2024-03-03 20:15:59 -0300

    teste

--- aula18/script.js
+++ aula18/script.js
@@ -1,3 +1,4 @@
 let times = ['gremio', 'gremio', 'gremio']
+times.push('Gremio')
 console.log(times)
 
---
+++ aula19/script.js
@@ -0,0 +1,6 @@
+function saudacao(nome){
+    console.log(`Bopm dia, ${nome}`)
+
+}
+saudacao(`Felipe`)
+saudacao(`Aysla`)
